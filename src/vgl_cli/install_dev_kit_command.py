f424768f3e67a7b16a5613265176ff12d5f6c23b
ivpel <ivan.pelykh@protonmail.com>
2023-05-02 14:10:49 +0300

    Updated install commands

--- src/vgl_cli/install_dev_kit_command.py
+++ src/vgl_cli/install_dev_kit_command.py
@@ -3,7 +3,7 @@ from .install_webdriver_command import check_version_and_download_driver
 from .install_standalone_command import check_req_and_install_selenium_server
 
 
-@click.command(name='install:dev-kit', help="Install local development kit (Webdriver, Selenium Server, etc)")
+@click.command(name='dev-kit', help="Install local development kit (Webdriver, Selenium Server, etc)")
 @click.option('-b', '--browser', type=click.Choice(['chrome', 'firefox', 'edge']), required=True)
 def install_dev_kit(browser):
     check_version_and_download_driver(browser)
