b37cb479007e104f0214f17d04495032bc055a2b
ivpel <ivan.pelykh@protonmail.com>
2023-12-06 15:29:58 +0200

    Changed way for configuration

--- src/vigilant/driver/__init__.py
+++ src/vigilant/driver/__init__.py
@@ -1,81 +1,32 @@
 import os
-import json
-from dotenv import load_dotenv, find_dotenv
+import yaml
+from pathlib import Path
 
-
-def check_env_variables_exist(variables):
-    missing_variables = []
-    for variable in variables:
-        if variable not in os.environ:
-            missing_variables.append(variable)
-
-    if missing_variables:
-        raise ValueError(f"Environment variables are missing: {', '.join(missing_variables)}")
-
-    return True
-
-
-def check_json_file_exist(filename):
-    current_dir = os.getcwd()
-
-    while True:
-        for root, dirs, files in os.walk(current_dir):
-            if filename in files:
-                return True
-
-        parent_dir = os.path.dirname(current_dir)
-        # Check if reached the root directory
-        if current_dir == parent_dir:
-            break
-        current_dir = parent_dir
-
-    return False
-
-
-def find_json_file(filename):
-    current_dir = os.getcwd()
-
-    while True:
-        for root, dirs, files in os.walk(current_dir):
-            if filename in files:
-                return os.path.join(root, filename)
-
-        parent_dir = os.path.dirname(current_dir)
-        # Check if reached the root directory
-        if current_dir == parent_dir:
-            break
-        current_dir = parent_dir
-
-    return None
-
-
-def set_env_variables_from_json(filename):
-    json_file_path = find_json_file(filename)
-    if json_file_path is None:
-        print(f"JSON file '{filename}' not found in the project's root directory or its subdirectories.")
-        return
-
-    with open(json_file_path, 'r') as file:
-        data = json.load(file)
-
+def set_env_variables_from_dict(data):
     for key, value in data.items():
         os.environ[key] = str(value)
 
+def set_env_variables_from_yaml(yaml_path):
+    with open(yaml_path, 'r') as file:
+        configs = yaml.safe_load(file)
 
-env_variables = ['BASE_URL', 'SELENIUM_HOST', 'SELENIUM_BROWSER', 'WAIT_TIMEOUT', 'LOGGER_LEVEL']
-json_configuration_file = 'vigilant.json'
-env_configuration_file = '.vigilant.env'
+    selenium_config = configs.get('selenium-configuration', {})
+    set_env_variables_from_dict(selenium_config)
 
-if find_dotenv(env_configuration_file):
-    print("Setting configuration from .vigilant.env file.")
-    load_dotenv(env_configuration_file)
-elif check_json_file_exist(json_configuration_file):
-    print("Setting configuration from .vigilant.json file.")
-    set_env_variables_from_json(json_configuration_file)
+    # Handle credentials
+    creds_file_path = configs.get('test-credentials', {}).get('file')
+    if creds_file_path and Path(creds_file_path).exists():
+        with open(creds_file_path, 'r') as creds_file:
+            credentials = yaml.safe_load(creds_file)
+        set_env_variables_from_dict(credentials)
+    else:
+        print(f"Credentials file '{creds_file_path}' not found.")
+
+CONFIG_YAML_FILE = 'vgl_config.yaml'
+
+if Path(CONFIG_YAML_FILE).exists():
+    print(f"Setting configuration from {CONFIG_YAML_FILE} file.")
+    set_env_variables_from_yaml(CONFIG_YAML_FILE)
 else:
-    print("Could not find nor `.vigilant.env` nor `vigilant.json` configuration files.")
-    print("If you don't want to use `.vigilant.env` nor `vigilant.json` configuration files - make sure, "
-          "that you provide all configuration data as environment variables through  the way that is acceptable for "
-          "you (pipeline configuration, custom bash script, etc.).")
-    if check_env_variables_exist(env_variables):
-        print("All mandatory environment variables exist.")
+    print(f"Could not find `{CONFIG_YAML_FILE}` configuration file.")
+    print("If you don't want to use the yaml configuration file, ensure you provide all configuration data as environment variables.")
